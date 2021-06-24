878e5461ece2b588f302738ccbefd689fae6e570
João Justo <joao.justo@roit.com.br>
2021-06-24 00:48:28 -0300

    fix(router): mode history

--- src/router.js
+++ src/router.js
@@ -5,20 +5,18 @@ import Principal from "./views/Principal.vue";
 Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   routes: [
     {
       path: "/",
       name: "principal",
       component: Principal
+    },
+    {
+      path: "/admin",
+      name: "admin",
+      component: () =>
+        import(/* webpackChunkName: "admin" */ "./views/Admin.vue")
     }
-    // {
-    // path: "/about",
-    //   name: "about",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () =>
-    //     import(/* webpackChunkName: "about" */ "./views/About.vue")
-    // }
   ]
 });
---
+++ src/views/Admin.vue
@@ -0,0 +1,369 @@
+<template>
+  <div>
+    <div class="top-background" />
+    <div class="margin-20">
+      <div class="text-center w-100">
+        <label class="label-style" for="" style="color: white"
+          >Selecione o número desejado, caso queira mais de um terá a opção
+          acessando um deles</label
+        >
+      </div>
+      <div class="d-flex pt-5 flex-wrap body">
+        <div v-for="(item, i) in returnNumbers" :key="i" class="p-2">
+          <div
+            :class="`card-number ${item.reserved ? 'reserved' : ''}`"
+            @click="openDialog(i + 1)"
+          >
+            <span>{{ item.number }}</span>
+            <!-- <small class="d-block mt-0 ml-2" v-if="item.reserved"
+              >VENDIDO</small
+            > -->
+            <small class="d-block ml-2" v-if="item.reserved">{{
+              item.user.split(" ")[0]
+            }}</small>
+          </div>
+        </div>
+      </div>
+    </div>
+    <el-dialog
+      :title="`Reservar número ${openedNumer}`"
+      :visible.sync="dialogVisible"
+      :width="returnModalWidth"
+    >
+      <div class="form-row">
+        <div class="col-12">
+          <label for="">Nome Completo</label>
+          <el-input placeholder="Insira seu nome" v-model="form.user">
+          </el-input>
+        </div>
+        <div class="col-12 mt-3">
+          <label for="">Telefone</label>
+          <el-input
+            v-mask="['(##) ####-####', '(##) #####-####']"
+            placeholder="Insira seu telefone"
+            v-model="form.celular"
+          >
+          </el-input>
+        </div>
+        <div class="col-12 mt-3">
+          <label for="">E-mail</label>
+          <el-input placeholder="Insira seu E-mail" v-model="form.email">
+          </el-input>
+        </div>
+        <div class="col-12 mt-3">
+          <label for=""
+            >Números Adicionais
+            <small class="d-block"
+              >(opcional deixe vazio caso seja apenas um)</small
+            >
+          </label>
+          <el-select
+            class="w-100"
+            v-model="form.numbers"
+            multiple
+            placeholder="Selecione"
+          >
+            <el-option
+              v-for="(item, i) in returnNumbers"
+              :key="i"
+              v-if="!item.reserved"
+              :label="item.number"
+              :value="item.number"
+            >
+            </el-option>
+          </el-select>
+        </div>
+        <div class="col-12 mt-3">
+          <label for="">Upload do comprovante (R$ 20,00 por número)</label>
+          <el-upload
+            class="upload-demo"
+            action=""
+            :on-change="fileInput"
+            :file-list="fileList"
+            v-loading="loadingUpload"
+            :auto-upload="false"
+            list-type="picture"
+          >
+            <el-button size="small" type="primary"
+              >Clique para fazer o upload</el-button
+            >
+            <div slot="tip" class="el-upload__tip">
+              aceito arquivos jpg/png
+            </div>
+          </el-upload>
+        </div>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancelar</el-button>
+        <el-button
+          :disabled="returnDisabled"
+          type="primary"
+          @click="finishPayment()"
+          >Confirmar</el-button
+        >
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { numbersCollection, FirebaseStorage } from "../common/firebase";
+
+export default {
+  name: "principal",
+  data() {
+    return {
+      loadingUpload: false,
+      reservedNumbers: [],
+      uploaded: false,
+      fileList: [],
+      dialogVisible: false,
+      form: {
+        user: "",
+        celular: "",
+        email: "",
+        numbers: [],
+        url: "",
+        status: "reserved"
+      },
+      openedNumer: "",
+      unsubscribe: ""
+    };
+  },
+  mounted() {
+    console.log("dasd");
+    this.unsubscribe = numbersCollection.onSnapshot(querySnapshot => {
+      querySnapshot.docChanges().forEach(change => {
+        const data = change.doc.data();
+
+        data.numbers.map(number => {
+          if (!this.reservedNumbers.find(currNumber => currNumber === number)) {
+            this.reservedNumbers.push({ number, user: data.user });
+          }
+        });
+      });
+    });
+    this.loadingTimer();
+  },
+  computed: {
+    returnModalWidth() {
+      console.log(document.body.clientWidth);
+      if (document.body.clientWidth > 600) {
+        return "40%";
+      } else {
+        return "80%";
+      }
+    },
+    returnNumbers() {
+      // return reservedNumbers
+      let arrNumbers = [];
+      let i;
+      for (i = 1; i <= 150; i++) {
+        const numberTEMP = this.reservedNumbers.find(
+          number => number.number === i
+        );
+        if (numberTEMP) {
+          arrNumbers.push({
+            number: i,
+            user: numberTEMP.user,
+            reserved: true
+          });
+        } else {
+          arrNumbers.push({
+            number: i,
+            user: "",
+            reserved: false
+          });
+        }
+      }
+      return arrNumbers;
+    },
+    returnDisabled() {
+      if (
+        this.form.user != "" &&
+        this.form.celular != "" &&
+        this.form.email != "" &&
+        this.form.numbers != [] &&
+        this.uploaded
+      ) {
+        return false;
+      }
+      return true;
+    }
+  },
+  methods: {
+    finishPayment() {
+      numbersCollection
+        .doc()
+        .set(this.form)
+        .then(() => {
+          this.dialogVisible = false;
+          this.uploaded = false;
+          this.form = {
+            user: "",
+            celular: "",
+            email: "",
+            numbers: [],
+            url: "",
+            status: "reserved"
+          };
+          this.fileList = [];
+          this.$message({
+            message: "Obrigado pela contribuição e boa sorte!!",
+            type: "success"
+          });
+        });
+    },
+    openDialog(i) {
+      this.form.numbers = [i];
+      this.openedNumer = i;
+      this.dialogVisible = true;
+    },
+    async fileInput(file) {
+      try {
+        if (file && file.name) {
+          this.processing = true;
+
+          var url = file.url;
+
+          const imgData = new FormData();
+          imgData.append("image", file);
+          const filePath = `comprovantes/${Date.now()}-${file.name}`;
+          const metadata = { contentType: file.type };
+          const that = this;
+
+          that.loadingUpload = true;
+
+          // await FirebaseStorage.ref().child(filePath);
+          fetch(url)
+            .then(res => res.blob())
+            .then(blob =>
+              FirebaseStorage.ref()
+                .child(filePath)
+                .put(blob, metadata)
+                .then(function() {
+                  that.uploaded = true;
+                  that.loadingUpload = false;
+
+                  FirebaseStorage.ref()
+                    .child(filePath)
+                    .getDownloadURL()
+                    .then(function(url) {
+                      that.form.url = url;
+                    });
+                })
+            );
+        }
+      } finally {
+        this.processing = false;
+      }
+    },
+    loadingTimer() {
+      var self = this;
+      this.$store.commit("SET_LOADING_STATUS", true);
+
+      setTimeout(function() {
+        self.$store.commit("SET_LOADING_STATUS", false);
+        self.show = true;
+      }, 2000);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Open+Sans");
+.v-modal {
+  display: none;
+}
+.el-dialog__wrapper {
+  background-color: rgba(0, 0, 0, 0.3);
+}
+img {
+  user-select: none;
+  -moz-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+}
+.top-background {
+  position: relative;
+  background: url("../assets/top_background.png") no-repeat center;
+  min-height: 180px;
+  background-size: cover;
+  z-index: 2;
+}
+.body {
+  justify-content: center;
+  margin-top: -50px;
+  padding-bottom: 30px;
+  padding-left: 50px;
+  padding-right: 50px;
+  z-index: 1;
+  background: rgb(62, 138, 244);
+  background: -moz-linear-gradient(
+    180deg,
+    rgba(62, 138, 244, 1) 0%,
+    rgba(41, 92, 163, 1) 70%,
+    rgba(31, 69, 122, 1) 100%
+  );
+  background: -webkit-linear-gradient(
+    180deg,
+    rgba(62, 138, 244, 1) 0%,
+    rgba(41, 92, 163, 1) 70%,
+    rgba(31, 69, 122, 1) 100%
+  );
+  background: linear-gradient(
+    180deg,
+    rgba(62, 138, 244, 1) 0%,
+    rgba(41, 92, 163, 1) 70%,
+    rgba(31, 69, 122, 1) 100%
+  );
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#3e8af4",endColorstr="#1f457a",GradientType=1);
+}
+.card-number {
+  cursor: pointer;
+  color: #fff;
+  background-color: rgba(32, 73, 130, 0.33);
+  border: 1px solid rgba(255, 255, 255, 0.4);
+  border-radius: 10px;
+  padding: 0 15px 15px 0;
+  height: 80px;
+  width: 80px;
+  span {
+    margin: 20px 0 0 5px;
+  }
+  &.reserved {
+    background-color: rgba(255, 73, 130, 0.8);
+    pointer-events: none;
+  }
+}
+
+.margin-20 {
+  margin-top: -20px;
+}
+
+@media only screen and (max-width: 640px) {
+  .margin-20 {
+    margin-top: -31px;
+  }
+  .top-background {
+    background: url("../assets/top_background.png") no-repeat contain;
+    min-height: 130px;
+    background-size: cover;
+  }
+  .body {
+    padding: 0 10px 30px 10px;
+  }
+}
+@media only screen and (min-width: 1600px) {
+  .margin-20 {
+    margin-top: 15px;
+  }
+  // .top-background {
+  //   background: url("../assets/top_background.png") no-repeat contain;
+  //   min-height: 130px;
+  //   background-size: cover;
+  // }
+}
+</style>
